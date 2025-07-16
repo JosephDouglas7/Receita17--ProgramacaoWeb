0ef454e415b32f141aa3c21f6cafbadb55a5294f
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2025-07-16 10:27:12 -0300

    Update page.js

--- page.js
+++ page.js
@@ -1,19 +1,1 @@
-import Form from "next/form"
 
-export default async function MovieForm(){
-
-    return (
-
-        <Form action="/movies">
-
-            <label htmlFor="idTitleSearchKey">Título</label>            
-
-            <input id="idTitleSearchKey" name="titleSearchKey"/>
-
-            <button type="submit">Pesquisar</button>
-
-        </Form>
-
-    )
-
-}
